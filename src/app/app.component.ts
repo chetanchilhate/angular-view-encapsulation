d1699a88a58e08a26b5f5b21f390a038a92e143e
chetanchilhate <chilhatechetan04@gmail.com>
2021-01-26 19:53:01 +0530

    default Emulated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, ViewEncapsulation } from "@angular/core";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class AppComponent {}
